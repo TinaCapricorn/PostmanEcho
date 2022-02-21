3e5b47d9078cbc7e24885e00a9296f787c910ff5
Klein Kristina <klein281293@gmail.com>
2022-02-21 21:46:27 +0700

    Check1

--- src/test/java/PostmanEchoTest.java
+++ src/test/java/PostmanEchoTest.java
@@ -17,7 +17,7 @@ class PostmanEchoTest {
                 .post("/post")
                 .then()
                 .statusCode(200)
-                .body("data", equalTo("Аркадий Паровозов"))
+                .body("data", equalTo("Василий Пупкин"))
         ;
     }
 
